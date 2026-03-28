0491922e5abd176ac2456d7ab951883fcd5902e0
Muhammed Sinan <sinan.46k@gmail.com>
2026-03-28 15:05:07 +0530

    wrapped every componenet with presence and auth context.

--- frontend/chit_chat/src/App.jsx
+++ frontend/chit_chat/src/App.jsx
@@ -4,18 +4,22 @@ import Signup from "./pages/SignUp";
 import VerifyOtp from "./pages/VerifyOtp";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
+import { PresenceProvider } from "./context/PresenceContext";
 
 function App() {
   return (
-    <Router>
-      <Toaster position="top-right" />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/verify-otp" element={<VerifyOtp />} />
-        <Route path="/chat" element={<Home />} />
-      </Routes>
-    </Router>
+    <PresenceProvider>
+      <Router>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/verify-otp" element={<VerifyOtp />} />
+          <Route path="/chat" element={<Home />} />
+        </Routes>
+      </Router>
+    </PresenceProvider>
+    
   );
 }
 
