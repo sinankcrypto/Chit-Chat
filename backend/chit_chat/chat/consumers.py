820b7fe1d4fb467c05a3cfe9073105dd3973ceda
Muhammed Sinan <sinan.46k@gmail.com>
2026-04-01 10:35:24 +0530

    Added notification event in presence consumer and added create notification for message send in chat consumer.

--- backend/chit_chat/chat/consumers.py
+++ backend/chit_chat/chat/consumers.py
@@ -3,6 +3,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
 from .models import ChatRoom, Message, ChatFile
+from notification.utils.notification_utils import create_notification
+from django.shortcuts import get_object_or_404
 import logging
 
 from chat.utils.presence import set_user_online, set_user_offline
@@ -22,6 +24,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
             return
 
         self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        self.room = await database_sync_to_async(
+            lambda: get_object_or_404(ChatRoom, id=self.room_id)
+        )()
         self.room_group_name = f"chat_{self.room_id}"
 
         # Check if user belongs to room
@@ -64,6 +69,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
             message_type = "text"
 
         saved_message = await self.save_message(message, message_type, file_id)
+
+        participants = await database_sync_to_async(
+            lambda: list(self.room.participants.exclude(id=self.user.id))
+        )()
+
+        for user in participants:
+            notification = await database_sync_to_async(create_notification)(
+                user,
+                self.user,
+                self.room,
+                saved_message.content
+            )
+            
+            await self.channel_layer.group_send(
+                f"user_{user.id}",
+                {
+                    "type": "notification_event",
+                    "notification_id": notification.id,
+                    "sender": self.user.username,
+                    "room_id": self.room.id,
+                    "message": saved_message.content or "Attachment"
+                }
+            )
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -192,4 +220,13 @@ class PresenceConsumer(AsyncWebsocketConsumer):
             "type": "presence",
             "user_id": event["user_id"],
             "status": event["status"]
+        }))
+
+    async def notification_event(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "notification",
+            "notification_id": event["notification_id"],
+            "sender": event["sender"],
+            "room_id": event["room_id"],
+            "message_id" : event["message_id"]
         }))
